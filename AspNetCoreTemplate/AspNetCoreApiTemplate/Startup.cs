29ad508b5b4b019fc05bf5cfd802a44dd73cbaba
政史 前田 <mamaeda@tech.softbank.co.jp>
2019-01-15 14:35:14 +0900

    update

--- AspNetCoreTemplate/AspNetCoreApiTemplate/Startup.cs
+++ AspNetCoreTemplate/AspNetCoreApiTemplate/Startup.cs
@@ -42,7 +42,7 @@ namespace AspNetCoreApiTemplate
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddApplicationInsights(app.ApplicationServices, LogLevel.Information);
+            loggerFactory.AddApplicationInsights(app.ApplicationServices, LogLevel.Debug);
 
             Migrate(app);
 
